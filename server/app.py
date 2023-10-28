183f456c6e238d0d340175ba0d99b38a99d07f76
phucnguyen02 <ndhp02@gmail.com>
2023-10-28 15:28:07 -0400

    Register/login routes setup

--- server/app.py
+++ server/app.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def index():
     return "Hello world"
 
+@app.route('/register')
+def register():
+    pass
+
+@app.route('/login')
+def login():
+    pass
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug = True)
