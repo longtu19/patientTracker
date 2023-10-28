5fcdbf2859245a08b1b097fa9283b57976a4294e
phucnguyen02 <ndhp02@gmail.com>
2023-10-28 16:08:25 -0400

    Connect to AWS server from backend

--- server/app.py
+++ server/app.py
@@ -1,10 +1,25 @@
 from flask import Flask
+from dotenv import load_dotenv
+import psycopg2
+import os
 
+load_dotenv()
 app = Flask(__name__)
+conn = psycopg2.connect(
+    host = os.environ.get("ENDPOINT"),
+    port = os.environ.get("PORT"),
+    user = os.environ.get("MASTER_USERNAME"),
+    password = os.environ.get("MASTER_PASSWORD"),
+    dbname = os.environ.get("DATABASE_NAME")
+)
+
+cur = conn.cursor()
+cur.execute(" CREATE TABLE IF NOT EXISTS person (id INT PRIMARY KEY,name VARCHAR(255), age INT);")
+conn.commit()
 
 @app.route('/')
 def index():
-    return "Hello world"
+    return "hi"
 
 @app.route('/register')
 def register():
