a68ed7a3d9e83ad34cdbe278df71b3878432dcea
Giang Nguyen <gn02.nguyen@gmail.com>
2023-11-01 17:06:38 -0400

    Implemented upload file button button and functionality and remove image assets.

--- frontend/patient-tracker/src/components/PatientHome/PatientHome.js
+++ frontend/patient-tracker/src/components/PatientHome/PatientHome.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import axios from 'axios';
 import './patient-home.css'
 import { Button } from "reactstrap";
 function upload() {
@@ -10,6 +11,51 @@ function update() {
 }
 
 function PatientHome(){
+    let [file, setFile] = React.useState({selectedFile: null})
+    const onFileChange = event => {
+        setFile({ selectedFile: event.target.files[0] });
+    }
+    const onFileUpload = () => {
+        // Create an object of formData
+        const formData = new FormData();
+ 
+        // Update the formData object
+        formData.append(
+            "myFile",
+            file.selectedFile,
+            file.selectedFile.name
+        );
+ 
+        // Details of the uploaded file
+        console.log(file.selectedFile);
+ 
+        // Request made to the backend api
+        // Send formData object
+        axios.post("api/uploadfile", formData);
+    };
+
+    const fileData = () => {
+        if (file.selectedFile) {
+            return (
+                <div className="uploadedFileInfo">
+                    <h3>File Detail:</h3>
+                    <p>File Name: {file.selectedFile.name}</p>
+                    <p>File Type: {file.selectedFile.type}</p>
+                    <p>
+                        Last Modified:{" "}
+                        {file.selectedFile.lastModifiedDate.toDateString()}
+                    </p>
+ 
+                </div>
+            );
+        } else {
+            return (
+                <div className="uploadedFileInfo">
+                </div>
+            );
+        }
+    };
+
     return (
         <div>
             <div className='info-card'>
@@ -27,8 +73,12 @@ function PatientHome(){
                     </div>
             </div>
             <div className='buttons'>
-                <Button onClick={upload}>Upload Medical Record</Button>
-                <Button onClick={update}>Update Medical Record</Button>
+                <div className = 'uploadButton'>
+                    <input className="uploadFileInput" type="file" onChange={onFileChange} />
+                    <Button onClick={upload}>Upload Medical Record</Button>
+                </div>
+                {fileData()}
+                <Button onClick={update}>Manually Fill out Medical Record</Button>
             </div>
         </div>
     )
