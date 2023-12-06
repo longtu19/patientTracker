80675e860e8811fa2926b5518ee8256110f7ed28
phucnguyen02 <ndhp02@gmail.com>
2023-12-06 16:25:19 -0500

    First unit tests

--- server/tests.py
+++ server/tests.py
@@ -6,8 +6,6 @@ import psycopg2
 import os
 import warnings
 load_dotenv()
-app = Flask(__name__)
-
 conn = psycopg2.connect(
     host = os.environ.get("ENDPOINT"),
     port = os.environ.get("PORT"),
@@ -18,21 +16,43 @@ conn = psycopg2.connect(
 
 warnings.filterwarnings(action="ignore", message="unclosed", category=ResourceWarning)
 
-test_object = {"email": "abc@gmail.com", "password": '123456', "first_name": "Bob", "last_name": "Americanman", \
-            "status": "patient", "birthday": "12/24/2002", "sex": "Male"}
+register_object = {
+    "email": "abcdef@gmail.com", 
+    "password": "123456", 
+    "first_name": "Bob", 
+    "last_name": "Americanman", 
+    "status": "patient", 
+    "birthday": "12/24/2002", 
+    "sex": "Male",
+    "role": "patient"
+}
+
+login_object = {
+    "email": "traudaica123@gmail.com", 
+    "password": "traudaica123", 
+}
 
 class FlaskTest(unittest.TestCase):
     def setUp(self) -> None:
         app.config['TESTING'] = True
         self.client = app.test_client()
 
-    # def test_register(self):
-    #     self.client.post('/register', data=test_object)
-    #     response = self.client.get('/register')
-    #     print("Request result: ", response.data)
-    def test_none(self):
+    def test_landing(self):
         response = self.client.get('/')
-        print("Request result: ", response.get_json())
+        self.assertEqual(response.data.decode(), "Welcome")
+
+    def test_register(self):
+        response = self.client.post('/register', json=register_object)
+        self.assertEqual(response.json["Result"], "Success")
+        
+        cur = conn.cursor()
+        query = "DELETE FROM system_user WHERE email = %s"
+        cur.execute(query, ("abcdef@gmail.com", ))
+        conn.commit()
+    
+    def test_login(self):
+        response = self.client.post('/login', json=login_object)
+        self.assertEqual(response.json["Result"], "Success")
 
 if __name__ == '__main__':
     unittest.main()
